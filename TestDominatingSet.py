1b0220362ed2b6285463662cc36180d97a3a93bb
THIOLIERE Martin <mthioliere@enseirb-matmeca.fr>
2022-10-06 13:08:22 +0200

    Adding a test sample to `TestDominatingSet.py`.

--- TestDominatingSet.py
+++ TestDominatingSet.py
@@ -57,7 +57,29 @@ def test_minimalDominatingSet():
         ]
     )
 
-    if sorted(r) != sorted(["v1", "v5", "v7", "v14"]):
+    if sorted(r) != sorted(["v2", "v5", "v7", "v14"]):
+        return False
+
+
+    r = minimalDominatingSet(
+        ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9"],
+        [
+            ("v1", "v2"),
+            ("v1", "v4"),
+            ("v2", "v3"),
+            ("v2", "v4"),
+            ("v2", "v5"),
+            ("v2", "v8"),
+            ("v3", "v5"),
+            ("v4", "v5"),
+            ("v4", "v6"),
+            ("v4", "v7"),
+            ("v5", "v6"),
+            ("v9", "v5")
+        ]
+    )
+
+    if sorted(r) != sorted(["v2", "v5", "v7"]):
         return False
 
     return True
